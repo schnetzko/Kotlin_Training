73995facb45038b9859c0425989dad3ddf9dbe63
Schnetzko <schnetzko@schnetzko.de>
2026-06-04 20:04:30 +0200

    rename DemoApplication.kt to PatientApplication.kt

--- patient/src/main/kotlin/com/medical/management/PatientApplication.kt
+++ patient/src/main/kotlin/com/medical/management/PatientApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class DemoApplication
+class PatientApplication
 
 fun main(args: Array<String>) {
-    SpringApplication.run(DemoApplication::class.java, *args)
+    SpringApplication.run(PatientApplication::class.java, *args)
 }
